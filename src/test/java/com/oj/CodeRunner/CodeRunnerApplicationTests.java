d062e0508805d6bc23d207ffbaf3f1d7306d6e5d
Mayank Maurya <mayank.maurya@proqio.com>
2024-01-27 02:28:01 +0530

    All Set to Work on initial code Runner backend app

---
+++ src/test/java/com/oj/CodeRunner/CodeRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oj.CodeRunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
